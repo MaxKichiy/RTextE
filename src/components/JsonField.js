59e748949776c68c18e32a24368fe209529d5e10
MaxKichiy <6aJIa6oJI1336@gmail.com>
2020-11-15 14:11:44 +0200

    formating JSON field add copy button

--- src/components/JsonField.js
+++ src/components/JsonField.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import FileCopyIcon from '@material-ui/icons/FileCopy';
 
 function JsonField({ formated }) {
+  const onClickCopyHandler = () => {
+    navigator.clipboard.writeText(JSON.stringify(formated));
+    alert('Copied to clipboard');
+  };
   return (
     <pre className='content__formated'>
+      <div className='content__button-copy'>
+        <FileCopyIcon className='content__button' onClick={onClickCopyHandler} />
+      </div>
       <code>{formated.length < 1 ? null : JSON.stringify(formated, undefined, 2)}</code>
     </pre>
   );
